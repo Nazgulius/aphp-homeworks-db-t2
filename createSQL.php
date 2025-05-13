0921c01aa340be41709a49202ba244f2849e8b03
Maksim Chalyh <nazgulius@yandex.ru>
2025-05-13 15:25:07 +0300

    fix

--- createSQL.php
+++ createSQL.php
@@ -15,24 +15,24 @@ try {
   $pdo->exec($sqlCreate);
 
   // обнуление AUTO_INCREMENT id = 1 
-  $sqlIncrementAuto = "ALTER TABLE shop AUTO_INCREMENT = 1";    
+  $sqlIncrementAuto = "ALTER TABLE shop AUTO_INCREMENT = 1";
   $pdo->exec($sqlIncrementAuto);
-  
+
   // добавление элементов в таблицу
   $sqlInsert = "INSERT INTO  shop (name, address) VALUES ('Lenta', 'Moscow') 
     ON DUPLICATE KEY UPDATE name = 'Lenta'";  // наполнение
   $pdo->exec($sqlInsert); // запись
   $sqlInsert = "INSERT INTO  shop (name, address) VALUES ('Karusel', 'Moscow, streat One, hous 1') 
-    ON DUPLICATE KEY UPDATE name = 'Karusel'";    
+    ON DUPLICATE KEY UPDATE name = 'Karusel'";
   $pdo->exec($sqlInsert);
   $sqlInsert = "INSERT INTO  shop (name, address) VALUES ('Da', 'Moscow, streat Two, hous 2') 
-    ON DUPLICATE KEY UPDATE name = 'Da'";    
+    ON DUPLICATE KEY UPDATE name = 'Da'";
   $pdo->exec($sqlInsert);
   $sqlInsert = "INSERT INTO  shop (name, address) VALUES ('Streat', 'Moscow, streat Three, hous 3') 
-    ON DUPLICATE KEY UPDATE name = 'Streat'";    
+    ON DUPLICATE KEY UPDATE name = 'Streat'";
   $pdo->exec($sqlInsert);
   $sqlInsert = "INSERT INTO  shop (name, address) VALUES ('Bingo', 'Moscow, streat Five, hous 5') 
-    ON DUPLICATE KEY UPDATE name = 'Bingo'";    
+    ON DUPLICATE KEY UPDATE name = 'Bingo'";
   $pdo->exec($sqlInsert);
 
 
@@ -66,24 +66,24 @@ try {
   $pdo->exec($sqlCreate);
 
   // обнуление AUTO_INCREMENT id = 1 
-  $sqlIncrementAuto = "ALTER TABLE client AUTO_INCREMENT = 1";    
+  $sqlIncrementAuto = "ALTER TABLE client AUTO_INCREMENT = 1";
   $pdo->exec($sqlIncrementAuto);
-  
+
   // добавление элементов в таблицу
   $sqlInsert = "INSERT INTO  client (name, phone) VALUES ('Thomas', '+79991118855') 
     ON DUPLICATE KEY UPDATE name = 'Thomas'";  // наполнение
   $pdo->exec($sqlInsert); // запись
   $sqlInsert = "INSERT INTO  client (name, phone) VALUES ('Anton', '+79992228855') 
-    ON DUPLICATE KEY UPDATE name = 'Anton'";    
+    ON DUPLICATE KEY UPDATE name = 'Anton'";
   $pdo->exec($sqlInsert);
   $sqlInsert = "INSERT INTO  client (name, phone) VALUES ('Otto', '+79992228811') 
-    ON DUPLICATE KEY UPDATE name = 'Otto'";    
+    ON DUPLICATE KEY UPDATE name = 'Otto'";
   $pdo->exec($sqlInsert);
   $sqlInsert = "INSERT INTO  client (name, phone) VALUES ('Kimi', '+79992227711') 
-    ON DUPLICATE KEY UPDATE name = 'Kimi'";    
+    ON DUPLICATE KEY UPDATE name = 'Kimi'";
   $pdo->exec($sqlInsert);
   $sqlInsert = "INSERT INTO  client (name, phone) VALUES ('Leonid', '+79993337733') 
-    ON DUPLICATE KEY UPDATE name = 'Leonid'";    
+    ON DUPLICATE KEY UPDATE name = 'Leonid'";
   $pdo->exec($sqlInsert);
 
 
@@ -119,24 +119,24 @@ try {
   $pdo->exec($sqlCreate);
 
   // обнуление AUTO_INCREMENT id = 1 
-  $sqlIncrementAuto = "ALTER TABLE product AUTO_INCREMENT = 1";    
+  $sqlIncrementAuto = "ALTER TABLE product AUTO_INCREMENT = 1";
   $pdo->exec($sqlIncrementAuto);
-  
+
   // добавление элементов в таблицу
   $sqlInsert = "INSERT INTO  product (name, price, count) VALUES ('apple', 50, 100) 
     ON DUPLICATE KEY UPDATE name = 'apple'";  // наполнение
   $pdo->exec($sqlInsert); // запись
   $sqlInsert = "INSERT INTO  product (name, price, count) VALUES ('pears', 60, 80) 
-    ON DUPLICATE KEY UPDATE name = 'pears'";    
+    ON DUPLICATE KEY UPDATE name = 'pears'";
   $pdo->exec($sqlInsert);
   $sqlInsert = "INSERT INTO  product (name, price, count) VALUES ('bananas', 120, 200) 
-    ON DUPLICATE KEY UPDATE name = 'bananas'";    
+    ON DUPLICATE KEY UPDATE name = 'bananas'";
   $pdo->exec($sqlInsert);
   $sqlInsert = "INSERT INTO  product (name, price, count) VALUES ('grapes', 200, 50) 
-    ON DUPLICATE KEY UPDATE name = 'grapes'";    
+    ON DUPLICATE KEY UPDATE name = 'grapes'";
   $pdo->exec($sqlInsert);
   $sqlInsert = "INSERT INTO  product (name, price, count) VALUES ('coconut', 400, 10) 
-    ON DUPLICATE KEY UPDATE name = 'coconut'";    
+    ON DUPLICATE KEY UPDATE name = 'coconut'";
   $pdo->exec($sqlInsert);
 
 
@@ -155,7 +155,7 @@ try {
   echo "Ошибка: " . $e->getMessage();
 }
 
-// создание и наполнение таблицы order:
+// создание и наполнение таблицы order2
 try {
   // Подключение
   $pdo = new PDO("mysql:host=localhost;dbname=testBase", 'root', '12345');
@@ -175,22 +175,22 @@ try {
   // удаляем лишнии записи
   $sql = "DELETE FROM order2";
   $pdo->exec($sql);
-  
+
   // обнуление AUTO_INCREMENT id = 1 
-  $sqlIncrementAuto = "ALTER TABLE order2 AUTO_INCREMENT = 1";    
+  $sqlIncrementAuto = "ALTER TABLE order2 AUTO_INCREMENT = 1";
   $pdo->exec($sqlIncrementAuto);
-  
+
 
   // добавление элементов в таблицу
   $sqlInsert = "INSERT INTO  order2 (created_at, seller, buyer) VALUES ('2025-05-10', 'Da', 'Thomas')";  // наполнение
   $pdo->exec($sqlInsert); // запись
-  $sqlInsert = "INSERT INTO  order2 (created_at, seller, buyer) VALUES ('2025-05-09', 'Lenta', 'Anton')";    
+  $sqlInsert = "INSERT INTO  order2 (created_at, seller, buyer) VALUES ('2025-05-09', 'Lenta', 'Anton')";
   $pdo->exec($sqlInsert);
-  $sqlInsert = "INSERT INTO  order2 (created_at, seller, buyer) VALUES ('2025-05-09', 'Karusel', 'Otto')";    
+  $sqlInsert = "INSERT INTO  order2 (created_at, seller, buyer) VALUES ('2025-05-09', 'Karusel', 'Otto')";
   $pdo->exec($sqlInsert);
-  $sqlInsert = "INSERT INTO  order2 (created_at, seller, buyer) VALUES ('2025-05-10', 'Karusel', 'Kimi')";    
+  $sqlInsert = "INSERT INTO  order2 (created_at, seller, buyer) VALUES ('2025-05-10', 'Karusel', 'Kimi')";
   $pdo->exec($sqlInsert);
-  $sqlInsert = "INSERT INTO  order2 (created_at, seller, buyer) VALUES ('2025-05-10', 'Bingo', 'Leonid')";    
+  $sqlInsert = "INSERT INTO  order2 (created_at, seller, buyer) VALUES ('2025-05-10', 'Bingo', 'Leonid')";
   $pdo->exec($sqlInsert);
 
 
@@ -234,9 +234,9 @@ try {
   $pdo->exec($sql);
 
   // обнуление AUTO_INCREMENT id = 1 
-  $sqlIncrementAuto = "ALTER TABLE order_product AUTO_INCREMENT = 1";    
+  $sqlIncrementAuto = "ALTER TABLE order_product AUTO_INCREMENT = 1";
   $pdo->exec($sqlIncrementAuto);
-  
+
   // добавление элементов в таблицу
   $sqlInsert = "INSERT INTO  order_product (name, price, count, created_at, seller, buyer) 
     VALUES ('apple', 50, 10, '2025-05-10', 'Da', 'Thomas') 
@@ -244,19 +244,19 @@ try {
   $pdo->exec($sqlInsert); // запись
   $sqlInsert = "INSERT INTO  order_product (name, price, count, created_at, seller, buyer) 
     VALUES ('pears', 60, 5, '2025-05-09', 'Lenta', 'Anton') 
-    ON DUPLICATE KEY UPDATE name = 'pears'";    
+    ON DUPLICATE KEY UPDATE name = 'pears'";
   $pdo->exec($sqlInsert);
   $sqlInsert = "INSERT INTO  order_product (name, price, count, created_at, seller, buyer) 
     VALUES ('bananas', 120, 50, '2025-05-09', 'Karusel', 'Otto') 
-    ON DUPLICATE KEY UPDATE name = 'bananas'";    
+    ON DUPLICATE KEY UPDATE name = 'bananas'";
   $pdo->exec($sqlInsert);
   $sqlInsert = "INSERT INTO  order_product (name, price, count, created_at, seller, buyer) 
     VALUES ('grapes', 200, 15, '2025-05-10', 'Karusel', 'Kimi') 
-    ON DUPLICATE KEY UPDATE name = 'grapes'";    
+    ON DUPLICATE KEY UPDATE name = 'grapes'";
   $pdo->exec($sqlInsert);
   $sqlInsert = "INSERT INTO  order_product (name, price, count, created_at, seller, buyer) 
     VALUES ('coconut', 400, 1, '2025-05-10', 'Bingo', 'Leonid') 
-    ON DUPLICATE KEY UPDATE name = 'coconut'";    
+    ON DUPLICATE KEY UPDATE name = 'coconut'";
   $pdo->exec($sqlInsert);
 
 
